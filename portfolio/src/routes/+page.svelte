16a3c6c769710c29a3342e3a0308d715af90a316
Simon Adrell <simon.adrell@gmail.com>
2024-10-26 14:01:44 +0200

    inlang

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import * as m from '$lib/paraglide/messages';
+</script>
+
+<h1>{m.hello_world({ name: 'Simon' })}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 style="text-3xl font-bold underline"></h1>
